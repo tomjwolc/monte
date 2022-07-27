f8d40561d61a1364a3a5d77f2d0c90b432b621b7
Tom Wolcott <tomjwolc@gmail.com>
2022-07-27 07:40:32 -0400

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,186 @@
+use std::marker::PhantomData;
+use rand::Rng;
+
+pub struct GameInfo {
+    pub input_size: usize, 
+    pub output_size: usize,
+    pub players: usize
+}
+
+pub trait Game<Choice> where Choice: Clone {
+    fn get_num_players(&self) -> usize;
+    fn get_turn(&self) -> usize;
+
+    fn get_choices(&self) -> Vec<Choice>;
+    fn choose(&mut self, choice: &Choice);
+    fn get_winner(&self) -> usize;
+    fn random_play(&mut self) -> usize {
+        let mut choices = self.get_choices();
+        let mut rng = rand::thread_rng();
+
+        while choices.len() > 0 {
+            self.choose(&choices[rng.gen_range(0..choices.len())]);
+
+            choices = self.get_choices();
+        }
+
+        self.get_winner()
+    }
+}
+
+#[allow(dead_code)]
+pub struct MCTS<Game_, Choice> where Choice: Clone, Game_: Game<Choice> + Clone {
+    players: usize,
+    __anoying: (PhantomData<Choice>, PhantomData<Game_>)
+}
+
+#[allow(dead_code)]
+impl<Game_, Choice> MCTS<Game_, Choice> where Choice: Clone, Game_: Game<Choice> + Clone {
+    pub fn new(initial_game_state: &Game_) -> Self {
+        let players = initial_game_state.get_num_players();
+
+        Self { players, __anoying: (PhantomData, PhantomData) }
+    }
+    
+    // returns the winner
+    fn mcts(&self, node: &mut Node<Game_, Choice>) -> usize {
+        if node.visits == 0.0 {
+            let winner = node.game_state.clone().random_play();
+
+            node.next = node.game_state.get_choices().iter().map(|choice| {
+                let mut next_game_state = node.game_state.clone();
+                next_game_state.choose(choice);
+
+                Node::new(next_game_state, Some(choice.clone()), self.players)
+            }).collect();
+
+            if node.next.len() == 0 {
+                node.winner = Some(winner);
+            }
+
+            return node.update(winner, self.players);
+        }
+
+        if let Some(winner) = node.winner {
+            return node.update(winner, self.players);
+        }
+
+        let next= node.best_next_index(
+            node.game_state.get_turn(), 
+            |wins, visits, parent_visits| UCB1(wins, visits, parent_visits)
+        ).expect("Tried to branch on dead end node");
+
+        let winner = self.mcts(&mut node.next[next]);
+
+        node.update(winner, self.players)
+    }
+
+    pub fn advise(&self, game_state: &Game_, cycles: usize) -> Option<Choice> where Choice: PartialEq {
+        let mut base_node = Node::default(game_state.clone(), self.players);
+
+        self.advise_with_tree(&mut base_node, cycles)
+    }
+
+    pub fn advise_with_tree(&self, tree: &mut Node<Game_, Choice>, cycles: usize) -> Option<Choice> where Choice: PartialEq {
+        for _ in 0..cycles {
+            self.mcts(tree);
+        }
+
+        let choice = match tree.best_next_index(
+            tree.game_state.get_turn(), 
+            |wins, visits, _| wins / visits
+        ) {
+            None => None,
+            Some(index) => {
+                let choice = tree.next[index].choice.clone().expect("can't unwrap choice at best_next_index");
+                tree.choose(&choice);
+
+                Some(choice)
+            }
+        };
+
+        choice
+    }
+}
+
+pub struct Node<Game_, Choice> where Game_: Game<Choice> + Clone, Choice: Clone {
+    winner: Option<usize>,
+    game_state: Game_,
+    choice: Option<Choice>,
+    wins: Vec<f64>,
+    visits: f64,
+    next: Vec<Node<Game_, Choice>>
+}
+
+impl<Game_, Choice> Node<Game_, Choice> where Game_: Game<Choice> + Clone, Choice: Clone {
+    fn new(game_state: Game_, choice: Option<Choice>, players: usize) -> Self {
+        Node { winner: None, game_state, choice, wins: vec![0.0; players], visits: 0.0, next: Vec::new() }
+    }
+
+    pub fn default(game_state: Game_, players: usize) -> Self {
+        Node { winner: None, game_state, choice: None, wins: vec![0.0; players], visits: 0.0, next: Vec::new() }
+    }
+
+    fn best_next_index(&self, player_id: usize, evaluator: fn(f64, f64, f64)-> f64) -> Option<usize> {
+        if self.next.len() == 0 { return None };
+
+        let mut best = (Vec::new(), -1.0);
+
+        for i in 0..self.next.len() {
+            let score = evaluator(self.next[i].wins[player_id - 1], self.next[i].visits, self.visits);
+
+            if score > best.1 {
+                best = (vec![i], score);
+            } else if score == best.1 {
+                best.0.push(i);
+            }
+        };
+        
+        let mut rng = rand::thread_rng();
+
+        Some(best.0[rng.gen_range(0..best.0.len())])
+    }
+
+    fn update(&mut self, winner: usize, players: usize) -> usize {
+        if winner > 0 { 
+            self.wins[winner - 1] += if let Some(_) = self.winner { 1000.0 } else { 1.0 }
+        } else { 
+            self.wins.iter_mut().for_each(|win| *win += if let Some(_) = self.winner { 1000.0 } else { 1.0 } / (players as f64)) 
+        }
+
+        self.visits += if let Some(_) = self.winner { 1000.0 } else { 1.0 };
+
+        winner
+    }
+
+    fn choose(&mut self, choice: &Choice) where Choice: PartialEq {
+        let chosen_node_index = self.next
+            .iter()
+            .position(|node| {
+                if let Some(node_choice) = node.choice.clone() { &node_choice == choice } else { false }
+            }).expect("The node does not include this choice");
+
+        *self = self.next.remove(chosen_node_index);
+    }
+}
+
+impl<Game_, Choice> std::fmt::Display for Node<Game_, Choice> where Game_: Game<Choice> + Clone, Choice: Clone + std::fmt::Debug {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.next.len() == 0 { return write!(f, "{{}}") };
+
+        let mut str = format!("[{}", self.next[0]);
+
+        for node in self.next[1..].iter() {
+            str += format!(", {}", node).as_str();
+        }
+
+        str += "]";
+
+        write!(f, "{{\"choice\": \"{:?}\", \"wins\": {:?}, \"visits\": {}, \"next\": {}}}", self.choice, self.wins, self.visits, str)
+    }
+}
+
+#[allow(non_snake_case)]
+fn UCB1(wins: f64, visits: f64, parent_visits: f64) -> f64 {
+    wins / (visits + 0.01) + (2.0 * parent_visits.ln() / (visits + 0.01)).sqrt()
+}
---
+++ tests/tic_tac_toe.rs
@@ -0,0 +1,137 @@
+extern crate monte;
+
+use monte::*;
+
+const WINS: [[usize; 3]; 8] = [
+    // rows
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // columns
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // diagnols
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+#[derive(Clone)]
+struct TTT {
+    board: [usize; 9],
+    turn: usize
+}
+
+impl TTT {
+    fn new() -> Self {
+        Self { board: [0; 9], turn: 1 }
+    }
+}
+
+impl Game<usize> for TTT {
+    fn get_num_players(&self) -> usize {
+        2
+    }
+
+    fn get_turn(&self) -> usize {
+        self.turn
+    }
+
+    fn get_choices(&self) -> Vec<usize> {
+        let mut choices = Vec::new();
+
+        if self.get_winner() != 0 { return choices };
+
+        for i in 0..9 {
+            if self.board[i] == 0 { choices.push(i) }
+        }
+
+        choices
+    }
+
+    fn choose(&mut self, choice: &usize) {
+        self.board[*choice] = self.turn;
+        self.turn = if self.turn == 1 { 2 } else { 1 };
+    }
+
+    fn get_winner(&self) -> usize {
+        let mut winner = 0;
+
+        if WINS.iter().any(|win| {
+            winner = self.board[win[0]];
+
+            self.board[win[0]] == self.board[win[1]] && 
+            self.board[win[1]] == self.board[win[2]] && 
+            self.board[win[0]] != 0
+        }) {
+            winner
+        } else {
+            0
+        }
+    }
+}
+
+impl std::fmt::Display for TTT {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let b: Vec<&str> = self.board.iter().map(|player_id| {
+            match player_id {
+                1 => "X",
+                2 => "O",
+                _ => " "
+            }
+        }).collect();
+
+
+        write!(f, "
+              {} | {} | {}
+            -------------
+              {} | {} | {}
+            -------------
+              {} | {} | {}
+        ", b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7], b[8])
+    }
+}
+
+#[test]
+fn play_tic_tac_toe() {
+    let ttt = TTT::new();
+
+    let mcts = MCTS::new(&ttt);
+
+    for i in 0..1 {
+        println!("Tree-less game #{}", i + 1);
+
+        play_ttt_without_tree(&ttt, &mcts);
+    }
+
+    for i in 0..1 {
+        println!("Tree game #{}", i + 1);
+
+        play_ttt_with_tree(&ttt, &mcts);
+    }
+}
+
+fn play_ttt_without_tree(ttt: &TTT, mcts: &MCTS<TTT, usize>) {
+    let mut clone = ttt.clone();
+    println!("{}", clone);
+
+    while let Some(choice) = mcts.advise(&clone, 1000) {
+        clone.choose(&choice);
+        println!("{}", clone);
+    }
+
+    println!("Winner: {}", clone.get_winner())
+}
+
+fn play_ttt_with_tree(ttt: &TTT, mcts: &MCTS<TTT, usize>) {
+    let mut clone = ttt.clone();
+    let mut tree = monte::Node::default(clone.clone(), clone.get_num_players());
+    println!("{} \n{}", tree, clone);
+
+    while let Some(choice) = mcts.advise_with_tree(&mut tree, 1000) {
+        clone.choose(&choice);
+        println!("{} \n{}", tree, clone);
+    }
+
+    println!("Winner: {}", clone.get_winner())
+}
